312ef8d1453fed5d9c7cbd94bda1f8e9182dd431
cuonglannguyen <94313142+cuonglannguyen@users.noreply.github.com>
2022-05-07 08:22:25 +0700

    Recommit: mistakenly deleted when commit lab03

---
+++ lab01/src/calculate.java
@@ -0,0 +1,32 @@
+import javax.swing.JOptionPane;
+public class calculate {
+    public static void main(String[] args){
+        String strNum1, strNum2;
+        strNum1 = JOptionPane.showInputDialog(null, "Please input the first number:",
+        "Input the first number", JOptionPane.INFORMATION_MESSAGE);
+        
+        strNum2 = JOptionPane.showInputDialog(null, "Please input the second number:",
+        "Input the second number", JOptionPane.INFORMATION_MESSAGE);
+        double num1 = Double.parseDouble(strNum1);
+        double num2 = Double.parseDouble(strNum2);
+        String sum_of_two_num = Double.toString(num1 + num2);
+        String diff_of_two_num = Double.toString(num1 - num2);
+        String product_of_two_num = Double.toString(num1 * num2);
+        String quotient_of_two_num;
+        if(num2 == 0){
+            quotient_of_two_num = "Not valid value of second number!";
+        } 
+        else{
+            quotient_of_two_num = Double.toString(num1 / num2);
+        }
+        String strNotification ="Sum of two numbers is: " + sum_of_two_num + "\n";
+        strNotification += "Difference of two numbers is: " + diff_of_two_num + "\n";
+        strNotification += "Product of two numbers is: " + product_of_two_num + "\n";
+        strNotification += "Quotient of two numbers is: " + quotient_of_two_num + "\n";
+        JOptionPane.showMessageDialog(null, strNotification, "Show the result", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+
+
+    
+    }
+}
